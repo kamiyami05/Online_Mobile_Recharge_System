ccbf9b3257b1f8dd990fc3e91f3b58cce7ccdc02
Bon-star <chuongdm26@gmail.com>
2025-11-10 16:10:20 +0700

    Add Recharge page
    cshtml n css

---
+++ sem3/Models/ModelViews/RechargePlans.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sem3.Models.ModelViews
+{
+    public class RechargePlans
+    {
+        public int PlanID { get; set; }
+        
+        public string PlanType { get; set; }
+
+
+    }
+}
