3e60db6e3c42b7ff438f8a0339736c736b7eec25
White Queen <zooxoox3@gmail.com>
2025-11-21 12:48:19 +0700

    Remove remaining parts of Role and little fix in Layout

--- sem3/Models/Entities/OnlineRechargeDB.Designer.cs
+++ sem3/Models/Entities/OnlineRechargeDB.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\zooxo\Downloads\D\sem3\Models\Entities\OnlineRechargeDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\zooxo\Downloads\rẻer\sem3\Models\Entities\OnlineRechargeDB.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
